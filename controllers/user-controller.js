4bb82c5e670540946705fb72a108c5ce0fb69d83
joe-white09 <joe_white09@yahoo.com>
2022-08-14 13:31:34 -0500

    added user model and api to post and update user

---
+++ controllers/user-controller.js
@@ -0,0 +1,71 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            })
+    },
+
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        })
+    },
+
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    },
+
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, {new: true, runValidators: true})
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: 'No user with this id' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: 'No user with this id' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    }
+};
+
+module.exports = userController;
---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: 'Username is required',
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: "Email is required",
+            unique: true,
+            trim: true,
+            validate: [validateEmail, 'Please fill a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
